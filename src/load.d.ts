1ec0f8925f1f4d173eb483aae701bc8c3e7eaa89
Joseph Sal <salagiannis.j@gmail.com>
2023-07-10 19:48:13 +0300

    silly

---
+++ src/load.d.ts
@@ -0,0 +1,4 @@
+import { PokemonInfo } from "./lib";
+export declare const POKEBALLS: String[];
+export declare const POKEMON: Map<string, PokemonInfo>;
+export declare function init(): void;
--- src/main.js
+++ src/main.js
@@ -2,11 +2,9 @@ import chalk from "chalk";
 import dayjs from "dayjs";
 import express from "express";
 import cors from "cors";
-import "./test";
-import { POKEBALLS, POKEMON, init } from "./load";
-
+import "./test.js";
+import { POKEBALLS, POKEMON, init } from "./load.js";
 console.log(chalk.magenta(`===== ${dayjs().format("HH:mm:ss")} =====`));
-
 const app = express(); //
 app.use(cors());
 const port = 3000; //
@@ -20,25 +18,23 @@ init();
 
 // Define your routes and server logic
 app.get("/pokeballs", (req, res) => {
-    res.send(JSON.stringify(POKEBALLS));
+  res.send(JSON.stringify(POKEBALLS));
 });
-
 app.get("/pokemon_list", (req, res) => {
-    res.send(Array.from(POKEMON.keys()));
+  res.send(Array.from(POKEMON.keys()));
 });
-
 app.get("/pokemon", (req, res) => {
-    const species = req.query.species;
-    console.log("Received query for", species);
-    if (typeof species !== "string") {
-        res.sendStatus(201);
-        return;
-    }
-    console.log("Res:", POKEMON.get(species));
-    res.send(POKEMON.get(species));
+  const species = req.query.species;
+  console.log("Received query for", species);
+  if (typeof species !== "string") {
+    res.sendStatus(201);
+    return;
+  }
+  console.log("Res:", POKEMON.get(species));
+  res.send(POKEMON.get(species));
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
